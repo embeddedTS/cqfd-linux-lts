69e2ed26a7fad8eb50679726352ead17b5f27ce4
Mark Featherston <mark@embeddedts.com>
2025-08-05 13:56:06 -0700

    build-tar: cleaned up style, output to build.tar.xz rather than custom name

--- build-scripts/build-tar.sh
+++ build-scripts/build-tar.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -e
+#!/bin/bash
+set -euo pipefail
 
 DEFCONFIG="$1"
 TARGET="$2"
@@ -17,27 +18,24 @@ fi
 
 cd linux
 make "$DEFCONFIG"
-make -j$(nproc)
-make -j$(nproc) "$TARGET"
+make -j"$(nproc)"
+make -j"$(nproc)" "$TARGET"
 
 TEMPDIR=$(mktemp -d)
 mkdir "${TEMPDIR}/boot/"
-cp arch/${ARCH}/boot/${TARGET} "${TEMPDIR}/boot"
-find arch/${ARCH}/boot/dts -regextype posix-extended -regex '.*/*ts[0-9]{4}.*\.dtb.*' -exec cp {} "${TEMPDIR}/boot" \;
-INSTALL_MOD_PATH="${TEMPDIR}" make modules_install
+cp "arch/$ARCH/boot/$TARGET" "$TEMPDIR/boot"
+find "arch/$ARCH/boot/dts" -regextype posix-extended -regex '.*/*ts[0-9]{4}.*\.dtb.*' -exec cp {} "${TEMPDIR}/boot" \;
+INSTALL_MOD_PATH="$TEMPDIR" make modules_install
 make headers_install INSTALL_HDR_PATH="${TEMPDIR}" # Optional
 cp .config "${TEMPDIR}"/boot/config # Optional
 
 if [ "${EXTERNAL_MODULE_WILC}" == "1" ]; then
-    CONFIG_WILC_SPI=m INSTALL_MOD_PATH="${TEMPDIR}" make M=../wilc3000-external-module modules modules_install
+    CONFIG_WILC_SPI=m INSTALL_MOD_PATH="$TEMPDIR" make M=../wilc3000-external-module modules modules_install
 fi
 
-TAR_NAME=linux-${DEFCONFIG%_defconfig}-"$(date +"%Y%m%d")"-"$(git describe --abbrev=8 --dirty --always)".tar.gz
-
-fakeroot sh -c "chmod 755 ${TEMPDIR};
-        chown -R root:root ${TEMPDIR};
-        tar czf $TAR_NAME -C ${TEMPDIR} .";
-
+fakeroot sh -c "chmod 755 $TEMPDIR;
+        chown -R root:root $TEMPDIR;
+        tar acf build.tar.xz -C $TEMPDIR ."
 rm -rf "${TEMPDIR}"
 
-echo "Tar is ready at $(realpath $TAR_NAME)"
+echo "Tar is ready at $(realpath build.tar.xz)"
